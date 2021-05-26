2285515cd751ec23c92aa00d0de85431fb99909f
Mayank Mani <many.mayank@gmail.com>
2021-05-27 01:18:01 +0530

    v2

--- server.js
+++ server.js
@@ -43,9 +43,9 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 }
 
-// app.get('*', (request, response) => {
-// 	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-// });
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.get('/robots.txt', (req, res) => {
 	res.sendFile(path.join(__dirname, 'client/build', 'robots.txt'));
